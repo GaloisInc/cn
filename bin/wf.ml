1707e9382e239fa4742dec93ff15408021529bb6
Gus O'Hanley <guso@galois.com>
2026-06-02 16:13:51 -0500

    Fix duplicate only flag

--- bin/wf.ml
+++ bin/wf.ml
@@ -20,7 +20,6 @@ let well_formed
       allow_split_magic_comments
       cache_status
       db_path
-      _only_function
   =
   let filename = Common.there_can_only_be_one filename in
   if cache_status then (
@@ -496,15 +495,6 @@ let cmd =
       & opt string ".cn/verification.db"
       & info [ "db-path" ] ~doc:"Path to verification database (for --cache-status)")
   in
-  let only_function_flag =
-    Arg.(
-      value
-      & opt (some string) None
-      & info
-          [ "only" ]
-          ~docv:"FUNCTION"
-          ~doc:"Only show cache status for this function (for --cache-status)")
-  in
   let wf_t =
     const well_formed
     $ Common.Flags.file
@@ -524,7 +514,6 @@ let cmd =
     $ Common.Flags.allow_split_magic_comments
     $ cache_status_flag
     $ db_path_flag
-    $ only_function_flag
   in
   let doc =
     "Runs CN's well-formedness check\n\
